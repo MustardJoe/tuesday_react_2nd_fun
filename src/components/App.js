d761f01b98a956effac6fc03e5162860432e5db2
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-20 15:10:47 -0700

    refactored to have all other comps besides container elsewhere

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Container from './container/Container';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Container />
+    </>
+  );
 }
-  
