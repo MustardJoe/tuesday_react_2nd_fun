7762b732fb2fe1fc678ba9a5ff3b21503c9602d1
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-20 22:59:08 -0700

    got random color thing working w button press

---
+++ src/components/randomcolor/RandomColor.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class RandomColor extends Component {
+  state = {
+    color: '',
+    trigger: new Date(),
+    img: '',
+  };
+
+  randomColor = () => {
+    const colors = ['tomato', 'dodgerblue', 'mediumspringgreen', 'gold', 'orange', 'purple', 'deeppink'];
+    const n = Math.floor(Math.random() * colors.length);
+
+    return colors[n];
+  }
+
+  // componentDidMount() {
+  //   this.intervalId = setInterval(() => {
+  //     this.setState({ color: this.randomColor(), trigger: new Date(), img: '' });
+  //   }, 2000);
+  // }
+
+  componentDidMount() {
+    this.setState({ color: this.randomColor(), trigger: new Date(), img: '' });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('hi');
+    // if(prevState.color === this.state.color) {
+    //   this.setState({ img: 'https://thumbs.gfycat.com/DemandingWelltodoGallinule-poster.jpg', color: null });
+    // }
+  }
+
+  componentWillUnmount() {
+    this.intervalId && clearInterval(this.intervalId);
+  }
+
+  colorChange = () => {
+    this.setState(
+      { color: this.randomColor(), img: null });
+  };
+  
+
+  render() {
+    const { color, img } = this.state;
+    const styles = {
+      background: img ? `center / contain url(${img})` : color,
+      width: '50em',
+      height: '50em',
+    };
+
+    return (
+      <>
+        <button onClick={this.colorChange}>Change color of color block</button>
+        <div style={styles}></div>
+      </>
+    );
+  }
+}
+
+export default RandomColor;
